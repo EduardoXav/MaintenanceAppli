9b27575f2f2baf6cec94cf50e49b15c3f66ec458
EduardoXav <eduardoxavier612@gmail.com>
2024-02-12 15:00:18 +0100

    18:44

--- movie-rental-java-master/src/main/java/movierental/Rental.java
+++ movie-rental-java-master/src/main/java/movierental/Rental.java
@@ -5,19 +5,51 @@ package movierental;
  */
 public class Rental {
 
-    private Movie _movie;
-    private int _daysRented;
+    private Movie movie;
+    private int daysRented;
 
     public Rental(Movie movie, int daysRented) {
-        _movie = movie;
-        _daysRented = daysRented;
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    static double determineAmounts(Rental rental) {
+        double thisAmount = 0;
+
+        //determine amounts for rental line
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (rental.getDaysRented() > 2)
+                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
+    static int addBonusForATwoDayNewReleaseRental(Rental rental) {
+        if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1)
+            return 1;
+        return 0;
+    }
+
+    static int frequentRenterPoints(Rental rental) {
+        return 1 + addBonusForATwoDayNewReleaseRental(rental);
     }
 
     public int getDaysRented() {
-        return _daysRented;
+        return daysRented;
     }
 
     public Movie getMovie() {
-        return _movie;
+        return movie;
     }
 }
